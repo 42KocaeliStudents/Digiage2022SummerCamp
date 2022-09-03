5eef29e43e80b49c349ff81f0c374a76af554827
canodis <cantosun45@gmail.com>
2022-09-03 22:08:12 +0300

    cam

--- DigiAge42/Assets/Script/Main/PickUpWeapon.cs
+++ DigiAge42/Assets/Script/Main/PickUpWeapon.cs
@@ -8,8 +8,7 @@ public class PickUpWeapon : MonoBehaviour
     public KeyCode dropKey = KeyCode.G;
     string weaponTag = "Weapon";
 
-    public List<GameObject> weapons;
-    public int maxWeapons = 2;
+    public GameObject weapon;
 
     public GameObject currentWeapon;
     public Transform hand;
@@ -21,65 +20,29 @@ public class PickUpWeapon : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            SelectWeapon(0);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            SelectWeapon(1);
-        }
-
         RaycastHit hit;
         Ray ray = new Ray(transform.position, transform.forward);
         Debug.DrawRay(transform.position, transform.forward);
 
         if (Physics.Raycast(ray, out hit))
         {
-            if (hit.transform.CompareTag(weaponTag) && Input.GetKeyDown(pickupKey) && weapons.Count < maxWeapons)
+            if (hit.transform.CompareTag(weaponTag) && Input.GetKeyDown(pickupKey))
             {         
-                weapons.Add(hit.collider.gameObject);
 
                 hit.collider.gameObject.SetActive(false);
-
                 hit.transform.parent = hand;
                 hit.transform.position = hand.transform.position;
                 hit.transform.rotation = hand.transform.rotation;
+                hit.collider.gameObject.SetActive(true);
             }
         }
 
         if (Input.GetKeyDown(dropKey) && currentWeapon != null)
         {
             currentWeapon.transform.parent = null;
-
             currentWeapon.transform.position = dropPoint.position;
-         
-            var weaponInstanceId = currentWeapon.GetInstanceID();
-            for (int i = 0; i < weapons.Count; i++)
-            {
-                if (weapons[i].GetInstanceID() == weaponInstanceId)
-                {
-                    weapons.RemoveAt(i);
-                    break;
-                }
-            }
-
+            currentWeapon.transform.rotation = new Quaternion(180, 0, 0, 0);
             currentWeapon = null;
-        }
-    }
-
-    void SelectWeapon(int index)
-    {
-        if (weapons.Count > index && weapons[index] != null)
-        {
-
-            if (currentWeapon != null)
-            {            
-                currentWeapon.gameObject.SetActive(false);
-            }
-            currentWeapon = weapons[index];
-            currentWeapon.SetActive(true);
         }
     }
 }
